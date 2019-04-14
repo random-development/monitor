b4d9c3402572c8b87bd0ff3e169b64e072f955ef
Filip Borkiewicz <filip@0x46.net>
2019-04-14 20:05:41 +0200

    Add an example test

---
+++ src/test/java/net/random_development/monitor/controller/ResourceControllerTest.java
@@ -0,0 +1,13 @@
+package net.random_development.monitor.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class ResourceControllerTest {
+
+    @Test
+    public void list() {
+        assertTrue(true);
+    }
+}
