49f7f33a52e4f210492cf94072276c4abc0362c4
Filip Borkiewicz <filip@0x46.net>
2019-05-31 22:31:47 +0200

    Insert mock data in a specified interval not just once

--- src/main/java/net/random_development/monitor/Application.java
+++ src/main/java/net/random_development/monitor/Application.java
@@ -2,8 +2,10 @@ package net.random_development.monitor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class Application {
 
     public static void main(String[] args) {
